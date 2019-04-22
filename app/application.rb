bcc8945434a81b455c694202f2b782655a3d6aea
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 14:08:45 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,24 @@
 class Application
-
-  @@items = ["Apples","Carrots","Pears"]
-
+ 
+  @@cart = []
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-      @@items.each do |item|
+  if @@cart == []
+      resp.write "Your cart is empty"
+  end
+    if req.path.match(/cart/)
+      @@cart.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      @@cart << @@items
+    
     else
-      resp.write "Path Not Found"
+      resp.write "Error"
     end
-
+ 
     resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
   end
 end
