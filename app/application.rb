1e995dacf9ff64078613f558c262fd57a8d034e3
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 14:17:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,15 @@
 class Application
- 
+  @@items = ["Apples","Carrots","Pears"]
   @@cart = []
  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
   if @@cart == []
       resp.write "Your cart is empty"
   end
+  
     if req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
